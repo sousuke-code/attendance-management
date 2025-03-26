f384f705302bbe80824d78bdbd7399efaa9ecb16
sousuke-code <msousuke1@gmail.com>
2025-03-26 23:20:00 +0900

    固定シフトの作成

--- src/actions/shift/createNewFixedShiftAction.ts
+++ src/actions/shift/createNewFixedShiftAction.ts
@@ -1,7 +1,8 @@
 "use server";
 import { redirect } from "next/navigation";
 import { db } from "../../../db";
-import { fixedShifts } from "@/db/schema/shift";
+import { fixedShifts, shifts } from "@/db/schema/shift";
+import { nextDay,isSameDay , addDays } from "date-fns";
 export default async function createNewFixedShiftAction(formData: FormData) {
     const teacherId = formData.get("teacherId");
     const studentId = formData.get("studentId");
@@ -18,9 +19,24 @@ export default async function createNewFixedShiftAction(formData: FormData) {
         subject: Number(subjectId),
         shiftId: Number(shiftTimeId)
     })
+
+    const today = new Date();
+    const daysToGenerate = 360;
+
+
+    for(let i = 0; i < daysToGenerate; i++){
+        const date = addDays(today, i);
+        if(date.getDay() === Number(weekday)){
+            await db.insert(shifts).values({
+                studentId: Number(studentId),
+                date: date.toLocaleString(),
+                subjectId: Number(subjectId),
+                shiftId: Number(shiftTimeId),
+                teacherId: Number(teacherId),
+            })
+        }
+    }
+
     redirect(`/admin/students/${studentId}`);
 
-
-
-    
 }
